4ba7abd82c19f7ec8d85625ea83d6caa0d144e07
Robert Kelley <bkelley13n@gmail.com>
2022-03-01 11:56:43 -0800

    update

--- nsb.bat
+++ nsb.bat
@@ -1,7 +1,5 @@
-cd %JET_HOME%\packages\oraclejet-storybook-addons\theme
-call npm run build
 cd %JET_HOME%\packages\oraclejet-preact
-call npm install
-call npm run storybook
+call npm run build-dev
+call npm run dev
 
 
